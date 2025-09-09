055b0ec430aa2e309986bcbe9a4b5e22954bddce
vijaya72569 <vijayaias2017@gmail.com>
2025-09-09 17:57:50 +0530

    Add Code in Index  file

--- WebAppAureGithub/obj/Debug/net8.0/WebAppAureGithub.AssemblyInfo.cs
+++ WebAppAureGithub/obj/Debug/net8.0/WebAppAureGithub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppAureGithub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f65d0b27ea6240387d51ce4e5dc60d12969d358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc2a66e34d9af55c909d36ebe81016af2792460")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppAureGithub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppAureGithub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
